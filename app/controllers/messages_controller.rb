842bed137c8bb6b5f5ab0e7e0f98f8a0f4b307ba
Abhishke Verma <nonuabi@gmail.com>
2021-11-25 12:49:21 +0530

    Display message usign partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,15 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      ActionCable.server.broadcast "chatroom_channel", foo: message.body
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel",
+                                   mod_message: message_render(message)
     end
   end
 
   private
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
+  end
   def message_params
     params.require(:message).permit(:body)
   end
